741831feddc8019be34c696e1be7726c3c0d2685
Sam Leitch <sam.leitch@calgaryscientific.com>
2013-09-23 16:46:53 -0600

    PWEB-3905 Returned VS2010 Samples to working condition.

--- DeployServer.bat
+++ DeployServer.bat
@@ -7,9 +7,9 @@
 
 if exist "%PUREWEB_HOME%\bin"\%1 goto deploy
 md "%PUREWEB_HOME%\bin"\%1 
-:deploy
-del /F /Q "%PUREWEB_HOME%\bin\"%1\*.* 
-copy %3\*.dll "%PUREWEB_HOME%\bin"\%1 
-copy %3\DDxService.exe "%PUREWEB_HOME%\bin"\%1 
 
-copy %2\plugin.xml "%PUREWEB_HOME%\conf\"%1-plugin.xml
+:deploy
+xcopy /YFDI %PUREWEB_LIBS%\C++\VS2010\lib\*.dll "%PUREWEB_HOME%\bin"\%1 
+xcopy /YFDI %3\DDxService.exe "%PUREWEB_HOME%\bin"\%1 
+
+xcopy /YFDI %2\plugin.xml "%PUREWEB_HOME%\conf\"%1-plugin.xml
