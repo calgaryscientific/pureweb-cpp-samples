cb2b6af8b02cc968ada70e7c1c634625a66ff3fb
Chris Jarabek <chris.jarabek@gmail.com>
2016-07-22 13:54:29 -0600

    PWEB-6185 Build both solo and non-solo C++ samples

--- Rakefile-windows.rb
+++ Rakefile-windows.rb
@@ -53,7 +53,11 @@ task :build,[:variant] => [:setup] do |t, args|
 	end
 	
 	if type == "release"
-		t.invoke_in_scope('build_release_2010')
+		if ENV["PUREWEB_BUILD_SUBREPO"] == "true"			
+			t.invoke_in_scope('build_release_solo_2010')
+		else
+			t.invoke_in_scope('build_release_2010')
+		end
 	end
 end
 
@@ -95,14 +99,12 @@ task :clean,[:variant]  do |t, args|
 end
 
 #### Internal Tasks
-task :build_debug_2010 => [:setup] do
-	#sh("\"#{DEVENV2010}\" \"#{VS2010_DDX_SLN}\" /Build \"Debug|x64\" /Out #{BUILD_DIR.gsub("/","\\")}\\logs\\ddx_debug_2010.log")	
-	sh("\"#{DEVENV2010}\" \"#{VS2010_SCRIBBLE_SLN}\" /Build \"Debug|x64\" /Out #{BUILD_DIR.gsub("/","\\")}\\logs\\scribble_debug_2010.log")	
+task :build_debug_2010 => [:setup] do	
+    sh("\"#{DEVENV2010}\" \"#{VS2010SLN}\" /Build \"DebugSamples|Any Cpu\" /Out #{BUILD_DIR.gsub("/","\\")}\\logs\\samples_debug_2010.log")    
 end
 
 task :build_release_2010 => [:setup] do
-	sh("\"#{DEVENV2010}\" \"#{VS2010_DDX_SLN}\" /Build \"Release|Any Cpu\" /Out #{BUILD_DIR.gsub("/","\\")}\\logs\\ddx_debug_2010.log")	
-	sh("\"#{DEVENV2010}\" \"#{VS2010_SCRIBBLE_SLN}\" /Build \"Release|Any Cpu\" /Out #{BUILD_DIR.gsub("/","\\")}\\logs\\scribble_debug_2010.log")	
+	sh("\"#{DEVENV2010}\" \"#{VS2010SLN}\" /Build \"ReleaseSamples|Any Cpu\" /Out #{BUILD_DIR.gsub("/","\\")}\\logs\\samples_release_2010.log")    
 end
 
 task :build_release => [:build_release_2010]
@@ -119,10 +121,28 @@ task :clean_release_2010 => [:setup] do
   sh("\"#{DEVENV2010}\" \"#{VS2010SLN}\" /Clean \"ReleaseSamples|Any Cpu\" /Out #{BUILD_DIR.gsub("/","\\")}\\logs\\samples_debug_2010.log")	
 end
 
-
 task :clean_release => [:clean_release_2010]
 
 task :clean_debug => [:clean_debug_2010]
+
+#The difference between these tasks below and those above, are that the ones below are based around DDxService.vcxproj and Scribble.vcxproj 
+#Whereas the above are built around DDxServiceDebug.vcxproj and ScribbleDebug.vcxproj.  The difference between these pairs of projects
+#is that the former refers to pre-built PureWeb components in DLLs found in PUREWEB_HOME, whereas the latter finds its references in 
+#source.  The tasks below are used for building the samples that get uploaded to S3.
+task :build_release_solo_2010 => [:setup] do	
+	sh("\"#{DEVENV2010}\" \"#{VS2010_DDX_SLN}\" /Build \"Release|x64\" /Out #{BUILD_DIR.gsub("/","\\")}\\logs\\ddx_debug_solo_2010.log")	
+	sh("\"#{DEVENV2010}\" \"#{VS2010_SCRIBBLE_SLN}\" /Build \"Release|x64\" /Out #{BUILD_DIR.gsub("/","\\")}\\logs\\scribble_debug_solo_2010.log")	
+end
+
+task :build_release_solo => [:build_release_solo_2010]
+
+task :clean_release_solo_2010 => [:setup] do
+  puts("Cleaning VS2010 Release SDK...")
+  sh("\"#{DEVENV2010}\" \"#{VS2010_DDX_SLN}\" /Clean \"Release|x64\" /Out #{BUILD_DIR.gsub("/","\\")}\\logs\\ddx_debug_solo_2010.log")	
+  sh("\"#{DEVENV2010}\" \"#{VS2010_SCRIBBLE_SLN}\" /Clean \"Release|x64\" /Out #{BUILD_DIR.gsub("/","\\")}\\logs\\scribble_debug_solo_2010.log")	
+end
+
+task :clean_release_solo => [:clean_release_solo_2010]
 
 task :cleantest do |t|
 	t.invoke_in_scope('clean', 'debug')
