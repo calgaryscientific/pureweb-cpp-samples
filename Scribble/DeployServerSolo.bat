c69bdf5c6f3eb85273b70bb5de71d91584742c4c
Kevin Robinson <kevin.nosnibor@gmail.com>
2017-01-27 12:53:50 -0700

    PWEB-6552 - Fix include paths in the VS2015 projects files that are acutally used by the build.

--- Scribble/DeployServerSolo.bat
+++ Scribble/DeployServerSolo.bat
@@ -9,6 +9,6 @@ echo "Creating directory %PUREWEB_HOME%\apps\%1..."
 md "%PUREWEB_HOME%\apps\%1" 
 
 :deploy
-echo "Sending application files to  %PUREWEB_HOME%\bin\%1..." 
+echo "Sending application files to  %PUREWEB_HOME%\app\%1..." 
 xcopy /YFDI %3\%1.exe "%PUREWEB_HOME%\apps\%1"
 xcopy /YFDI %2\service.json "%PUREWEB_HOME%\apps\%1"
